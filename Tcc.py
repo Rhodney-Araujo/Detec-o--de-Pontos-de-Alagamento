228e80afa7a6cc558ed3c5d02cb13bb0b0abea3b
Rhodney-Araujo <92754637+Rhodney-Araujo@users.noreply.github.com>
2024-12-06 14:16:19 -0300

    Update Tcc.py

--- Tcc.py
+++ Tcc.py
@@ -5,7 +5,6 @@ import time
 from datetime import datetime, timedelta
 from supabase import create_client, Client
 
-
 url = 'https://dxruudjipgsrkqdycttc.supabase.co'
 key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4cnV1ZGppcGdzcmtxZHljdHRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAyODE0NzcsImV4cCI6MjAzNTg1NzQ3N30.wtyKGyfBOKKDuKs63cLkPnwMHn4vw1NCELNUMVFeYTg'
 
@@ -29,7 +28,6 @@ if arduino_port:
         timeout=1  # Adicione um timeout para evitar travamento em leituras
     )
     print(f"Conectado ao Arduino na porta {arduino_port}")
-
 
 # Variável para armazenar o ID do último registro inserido
 ultimo_id_inserido = None
@@ -71,11 +69,13 @@ def dados_persistentes_true(supabase):
         dados = {
             'AlagamentoPersistente': "Sim",
         }
-        supabase.table('sensor').update(dados).eq('Código', ultimo_id_inserido).execute()
-        print(f"Alagamento persistente atualizado para sim no registro" , {ultimo_id_inserido} )
-    else:
-        print("Nenhum ID de registro para atualizar.")
-
+        try:
+            supabase.table('sensor').update(dados).eq('Código', ultimo_id_inserido).execute()
+            print(f"Alagamento persistente atualizado para sim no registro" , {ultimo_id_inserido} )
+        except Exception as e:
+            print(f"Erro ao atualizar dados no Supabase: {e}")
+        finally:
+            print("Processo de atualização de dados finalizado.")
 
 def dados_persistentes_false(supabase):
     global ultimo_id_inserido
@@ -102,6 +102,33 @@ def verif_sinal(sinal):
         valores_sensor.append(0)
     print(f"Lista atualizada: {valores_sensor}")
 
+def dados_persistentes_false(supabase):
+    global ultimo_id_inserido
+    if ultimo_id_inserido is not None:
+        data_hora_atual = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        hora_final = datetime.now()
+        duracao = hora_final - hora_inicial
+        duracao_formatada = str(duracao).split('.')[0]  # Remove milissegundos
+        dados = {
+            'AlagamentoPersistente': "Não",
+            'DataHora': data_hora_atual,
+            'HoraFinal': hora_final.strftime('%H:%M:%S'),  # Converter datetime para string
+            'Duração' : duracao_formatada
+        }
+        supabase.table('sensor').update(dados).eq('Código', ultimo_id_inserido).execute()
+        print(f"Alagamento persistente atualizado para False no registro {ultimo_id_inserido} em:", data_hora_atual)
+        ultimo_id_inserido = None
+    else:
+        print("Nenhum ID de registro para atualizar.")
+
+
+def verif_sinal(sinal):
+    if sinal == 1:
+        valores_sensor.append(1)
+    else:
+        valores_sensor.append(0)
+    print(f"Lista atualizada: {valores_sensor}")
+
 
 valores_sensor = []
 positividade = False #define que a maioria dos valores foram positvos na lista
@@ -114,8 +141,8 @@ while True:
         sinal = int(message)
         verif_sinal(sinal)
 
-        # Verificação inicial com 55 leituras
-        if len(valores_sensor) == 55: #55 verificações
+        # Verificação inicial com 10 leituras
+        if len(valores_sensor) == 55: #serão 55 verificações
             positivos = valores_sensor.count(1)
             negativos = valores_sensor.count(0)
 
@@ -125,19 +152,21 @@ while True:
                 print("Maioria positiva nas primeiras 55 leituras. Inserindo no banco e aguardando 10 minutos.")
                 valores_sensor = []
                 arduino.close()
-                time.sleep(600) #10 minutos
-                arduino = serial.Serial(port = arduino_port, baudrate = 9600)
+                time.sleep(600) #serão 10 minutos
+                arduino = serial.Serial(port=arduino_port, baudrate=9600)
+
 
             else:
                 print("Maioria negativa nas primeiras 55 leituras. Reiniciando...")
                 valores_sensor = []
                 arduino.close()
-                time.sleep(600) #10 minutos
-                arduino = serial.Serial(port = arduino_port, baudrate = 9600)
+                time.sleep(600) #serão 10 minutos
+                arduino = serial.Serial(port=arduino_port, baudrate=9600)
 
-        # Após a pausa, fazer 51 leituras e continuar o fluxo
+        # Após a pausa, fazer 55 leituras e continuar o fluxo
         if positividade and len(valores_sensor) >= 51:
             positivos = valores_sensor.count(1)
+
             negativos = valores_sensor.count(0)
 
             if positivos > negativos:
@@ -151,8 +180,8 @@ while True:
 
             valores_sensor = []
             arduino.close()
-            time.sleep(600) # Esperar 10 minutos antes de começar uma nova rodada de 55 leituras
-            arduino = serial.Serial(port = arduino_port, baudrate = 9600)
+            time.sleep(600) # Esperar 10 minutos (600 no time.sleep) antes de começar uma nova rodada de 55 leituras
+            arduino = serial.Serial(port=arduino_port, baudrate=9600)
 
 
     time.sleep(2)
